399a7dc492cb11a997c16f0342db6921c630f14f
Yaying Fu <Yaying.Fu@gmail.com>
2022-10-26 15:49:00 -0400

    Add three files

---
+++ Third.py
@@ -0,0 +1,21 @@
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+a = 4
+A = "Sally"
+
+print(a)
+print(A)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+
+x = "Python is awesome"
+print(x)
---
+++ second.py
@@ -0,0 +1,9 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
