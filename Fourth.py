308034dbb355b74b8341d7a3cbcba0464f6314c0
Yaying Fu <Yaying.Fu@gmail.com>
2022-10-27 10:16:37 -0400

    Add new comment and then push

--- Fourth.py
+++ Fourth.py
@@ -1,8 +1,21 @@
 x = "awesome"
 
 def myfunc():
+  x = "fantastic"
   print("Python is " + x)
 
 myfunc()
 
+print("Python is " + x)
+
+
+
+
+y=2j
+
+print(type(y))
+
 ## Want to pull this edited file to the local repository
+
+
+### Just add new comment and then push
